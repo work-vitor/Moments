259ddf4c077fcdaf477321b9ad3ec844b64b0ef6
Joao Vitor <joao@logstore.com.br>
2025-04-16 21:45:36 -0300

    feat: add validation to the form

--- src/app/components/moments-form/moments-form.component.ts
+++ src/app/components/moments-form/moments-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moments-form',
@@ -8,10 +9,34 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MomentsFormComponent implements OnInit {
 
   @Input() btnText!: string
+  
+  momentForm!: FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    })
+  }
+
+  get title(){
+    return this.momentForm.get('title')!
+  }
+  get description(){
+    return this.momentForm.get('description')!
+  }
+  
+
+  submit(){
+    if(this.momentForm.invalid){
+      return
+    }
+
+    console.log('enviou')
   }
 
 }
